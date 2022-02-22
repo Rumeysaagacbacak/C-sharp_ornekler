dfd6075bcd6dca0aee8f3b84d93fa27d6de7fb65
rumeysa <rumey@DESKTOP-M5AHE8G>
2022-02-22 15:12:31 +0300

    0-100-arasındamı-bulma

---
+++ Alıştırma5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Alıştırma5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen sayının 0-100 arasında olup olmadığını kontrol eden program
+            
+            Console.WriteLine("**** Alıştırma 5 **** ");
+            int sayi =0;
+            Console.Write("Lütfen sayı giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if(sayi>=0 && sayi<=100)
+            {
+                Console.WriteLine("Girilen sayı 0-100 arasında");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı 0-100 arasında değil");
+            }
+            Console.ReadKey();
+        }
+    }
+}
