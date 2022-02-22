02c543391b0c1ce47cff130bb671a26dba44915d
rumeysa <rumey@DESKTOP-M5AHE8G>
2022-02-22 15:06:40 +0300

    ad_soyad-yazdirma

---
+++ Alistirma1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alistirma1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ad ve soyad bilgisini string değişenlere alarak ekranda yazdırma
+            
+            Console.WriteLine("**** Alıştırma 1 **** ");
+            string ad;
+            Console.Write("Adınız : ");
+            ad=Console.ReadLine();
+            string soyad;
+            Console.Write("Soyadınız : ");
+            soyad=Console.ReadLine();
+            Console.WriteLine("Hoşgeldin " + ad +" "+ soyad);
+            Console.ReadKey();
+            
+        }
+    }
+}
