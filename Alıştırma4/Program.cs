a4fda1774d5c82fa3d8b1adeaeffab1ad7a3acb0
rumeysa <rumey@DESKTOP-M5AHE8G>
2022-02-22 15:10:43 +0300

    tek-cift-bulma

---
+++ Alıştırma4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alıştırma4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Alıştırma 4 **** ");
+            int sayi;
+            Console.Write("Lütfen sayı giriniz :  ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if(sayi%2 == 0)
+            {
+               Console.Write(sayi+" "+"sayısı çift sayıdır");
+            }
+            else
+            {
+                Console.Write(sayi+" "+"sayısı tek sayıdır");
+            }
+            Console.ReadKey();
+        }
+    }
+}
