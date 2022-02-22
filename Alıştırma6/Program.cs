2702ed883bef472795d7df97c10cb8ac2a9df1fe
rumeysa <rumey@DESKTOP-M5AHE8G>
2022-02-22 15:13:36 +0300

    vucut-kitle-indeksi

---
+++ Alıştırma6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Alıştırma6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vücut Kitle İndeksi hesaplayan program
+            
+            Console.WriteLine("**** Alıştırma 6 **** ");
+            double boy ;
+            Console.Write("Lütfen boyunuzu (m)(örnek=1,60) giriniz : ");
+            boy = Convert.ToDouble(Console.ReadLine());
+            double kilo ;
+            Console.Write("Lütfen kilonuzu giriniz (kg) giriniz : ");
+            kilo = Convert.ToDouble(Console.ReadLine());
+            double indeks=0;
+            indeks=kilo/(boy*boy );
+            Console.WriteLine("Vücut kitle indexiniz : {0}",indeks);
+            if(indeks <18)
+            {
+                Console.WriteLine("Zayıf");
+            }
+            else if(indeks>=18 && indeks<25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if(indeks >=25 && indeks<30)
+            {
+                Console.WriteLine("Kilolu");
+            }
+            else if(indeks >=30 && indeks<33)
+            {
+                Console.WriteLine("Obez");
+            }
+            else
+            {
+                Console.WriteLine("Ciddi Obez");
+            }
+            Console.ReadKey();           
+        }
+    }
+}
