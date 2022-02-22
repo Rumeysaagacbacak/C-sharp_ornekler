60f6e380fe5da1fdcc057983081149d9e465a62e
rumeysa <rumey@DESKTOP-M5AHE8G>
2022-02-22 15:14:23 +0300

    cumledi-harf-sayisi

---
+++ Alıştırma7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Alıştırma7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cümledeki harf sayısını bulan program
+            Console.WriteLine("**** Alıştırma 7 **** ");
+            Console.WriteLine("Lütfen cümle giriniz :");
+            string cumle =Console.ReadLine();
+            string liste =  "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+                           +"abcçdefgğhiıjklmnoöprsştuüvyz";
+            int sayac=0;
+            for (int i = 0; i < cumle.Length; i++)
+            {if (liste.Contains(cumle[i]))
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine("Girdiğiniz cümlede "+sayac+ " harf vardır");
+            Console.ReadKey();
+        }
+    }
+}
