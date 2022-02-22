61b1ef8d01a2189b2e28b4ba7f0258b5b7ed8ae2
rumeysa <rumey@DESKTOP-M5AHE8G>
2022-02-22 15:07:57 +0300

    2-sayi-toplama

---
+++ Alıştırma2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Alıştırma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan 2 sayı isteyerek bu sayıların toplamını bulan program
+            Console.WriteLine("**** Alıştırma 2 **** ");
+            int sayi1,sayi2,toplam = 0;
+            Console.WriteLine("Lütfen 2 sayı giriniz : ");
+            Console.Write("1. sayı : ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. sayı : ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            toplam =sayi1+sayi2;
+            Console.WriteLine( sayi1 +" "+"ve"+" "+sayi2 +" "+"Sayılarının toplamı = {0}",toplam);
+            Console.ReadKey();
+        }
+    }
+}
